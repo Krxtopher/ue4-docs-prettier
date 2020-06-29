b1e685375ba9cba72d4d7968b8db173a839710f7
Kris Schultz <schultkr@amazon.com>
2020-06-29 16:09:52 -0700

    Remove @updateURL and @downloadURL directives.

--- ue4-docs-prettier.user.js
+++ ue4-docs-prettier.user.js
@@ -2,8 +2,6 @@
 // @name         Unreal Engine API Docs Prettier
 // @namespace    https://kschultz-web-misc.s3-us-west-2.amazonaws.com
 // @version      0.1
-// @updateURL    https://kschultz-web-misc.s3-us-west-2.amazonaws.com/tamper-monkey/ue2-docs-prettier/ue4-docs-prettier.js
-// @downloadURL  https://raw.githubusercontent.com/Krxtopher/ue4-docs-prettier/main/ue4-docs-prettier.js
 // @description  Improves formatting of Unreal Engine API documentation.
 // @author       Kris Schultz
 // @match        https://docs.unrealengine.com/en-US/API/*
@@ -25,6 +23,6 @@
     const cssLink = document.createElement('link');
     cssLink.rel = 'stylesheet';
     cssLink.type = 'text/css';
-    cssLink.href = 'https://kschultz-web-misc.s3-us-west-2.amazonaws.com/tamper-monkey/ue2-docs-prettier/ue4-docs-theme1.css';
+    cssLink.href = 'https://kschultz-web-misc.s3-us-west-2.amazonaws.com/tampermonkeyXX/ue2-docs-prettier/ue4-docs-theme1.css';
     document.head.append(cssLink);
 })();
