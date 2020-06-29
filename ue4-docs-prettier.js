71d2eef0f62fa4591440161d2065407c5e39dac8
Kris Schultz <schultkr@amazon.com>
2020-06-29 13:10:03 -0700

    Add Tamper Monkey @downloadURL.

--- ue4-docs-prettier.js
+++ ue4-docs-prettier.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name         Unreal Engine API Docs Prettier
 // @namespace    https://kschultz-web-misc.s3-us-west-2.amazonaws.com
-// @version      0.2
-// @updateurl    https://kschultz-web-misc.s3-us-west-2.amazonaws.com/tamper-monkey/ue2-docs-prettier/ue4-docs-prettier.js
+// @version      0.1
+// @updateURL    https://kschultz-web-misc.s3-us-west-2.amazonaws.com/tamper-monkey/ue2-docs-prettier/ue4-docs-prettier.js
+// @downloadURL  https://raw.githubusercontent.com/Krxtopher/ue4-docs-prettier/main/ue4-docs-prettier.js
 // @description  Improves formatting of Unreal Engine API documentation.
 // @author       Kris Schultz
 // @match        https://docs.unrealengine.com/en-US/API/*
