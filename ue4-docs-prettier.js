50342979e49b312c7cb8c85a1f6e0a6005841132
Kris Schultz <schultkr@amazon.com>
2020-06-29 12:50:13 -0700

    Rename files.

---
+++ ue4-docs-prettier.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Unreal Engine API Docs Prettier
+// @namespace    https://kschultz-web-misc.s3-us-west-2.amazonaws.com
+// @version      0.1
+// @description  Improves formatting of Unreal Engine API documentation.
+// @author       Kris Schultz
+// @match        https://docs.unrealengine.com/en-US/API/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    // Collapse sections.
+    document.querySelectorAll('.expanded').forEach(el => {
+        // Keep only the "Syntax" section open by default.
+        if (el.firstElementChild.textContent != 'Syntax') {
+            el.onclick();
+        }
+    });
+
+    // Inject better CSS.
+    const cssLink = document.createElement('link');
+    cssLink.rel = 'stylesheet';
+    cssLink.type = 'text/css';
+    cssLink.href = 'https://kschultz-web-misc.s3-us-west-2.amazonaws.com/tamper-monkey/ue2-docs-prettier/ue4-docs-theme1.css';
+    document.head.append(cssLink);
+})();
